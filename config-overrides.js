40a151f926c3b00db79e1f193e3c926c9162cb0a
Kashif <kashif@somewhere>
2025-12-26 05:16:21 +0000

    fix: explicitly add PostCSS plugins to webpack config

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,12 @@
 // Overrides create-react-app webpack configs without ejecting
 // https://github.com/timarney/react-app-rewired
 
-const { useBabelRc, override } = require("customize-cra");
-module.exports = override(useBabelRc());
+const { useBabelRc, override, addPostcssPlugins } = require("customize-cra");
+
+module.exports = override(
+  useBabelRc(),
+  addPostcssPlugins([
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ])
+);
