08daefbb1bccc54217be00f592280f89b2558e9d
Kashif <kashif@somewhere>
2025-12-26 05:20:42 +0000

    revert: let CRA v5 handle PostCSS automatically via postcss.config.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,10 @@
 // Overrides create-react-app webpack configs without ejecting
 // https://github.com/timarney/react-app-rewired
 
-const { useBabelRc, override, addPostcssPlugins } = require("customize-cra");
+const { useBabelRc, override } = require("customize-cra");
 
+// CRA v5 automatically picks up postcss.config.js
+// No need to explicitly add PostCSS plugins here
 module.exports = override(
-  useBabelRc(),
-  addPostcssPlugins([
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ])
+  useBabelRc()
 );
