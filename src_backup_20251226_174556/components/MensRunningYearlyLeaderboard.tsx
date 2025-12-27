e753c7bd6ef6b629ba85baaa82a1276afc3b3da8
Kashif <kashif@somewhere>
2025-12-27 21:37:25 +0000

    clean up

--- src_backup_20251226_174556/components/MensRunningYearlyLeaderboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LeaderboardTable from './LeaderboardTable';
-
-const MensRunningYearlyLeaderboard: React.FC = () => {
-  return <LeaderboardTable endpoint="/api/yearly_mens_running_leaders" title="Men's Yearly Running Leaders" />;
-};
-
-export default MensRunningYearlyLeaderboard;
